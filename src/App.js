8f8117cf400d544bcb5234f01948ef30ecf8de9b
dongdongdongk <dhk9309@gmail.com>
2024-01-17 14:25:23 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import GoalList from './components/GoalList';
+
+const courseGaols = [
+  { id: 'cg1', text: 'Finish the Course1111111111111' },
+  { id: 'cg2', text: 'Finish the Course2222222222222' },
+  { id: 'cg3', text: 'Finish the Course3333333333333' }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList goals ={courseGaols}/>
     </div>
   );
 }
